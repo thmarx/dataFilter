badedf165e305bc74050e99f4dc61775a3de542c
thmarx <thmarx@gmx.net>
2012-05-06 15:30:34 +0200

    remove refactored classes

--- src/main/java/net/mad/data/datafilter/helper/Predicate.java
+++
@@ -1,5 +0,0 @@
-package net.mad.data.datafilter.helper;
-
-public interface Predicate<T> {
-	public boolean apply (T taget);
-}
--- src/main/java/net/mad/data/datafilter/helper/ValueAccessorFunktion.java
+++
@@ -1,5 +0,0 @@
-package net.mad.data.datafilter.helper;
-
-public interface ValueAccessorFunktion<T, V> {
-	public V apply (T type);
-}
